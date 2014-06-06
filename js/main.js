33ab578f337d0ca77219d85322f36dbc05df26a5
r18 <twkgr424@yahoo.co.jp>
2014-06-06 11:35:47 +0900

    Add board

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 WIDTH = 640;
 HEIGHT = 480;
+X_LINES = 9;
+Y_LINES = 9;
+BOARD_OFFSET_X = 30;
+BOARD_OFFSET_Y = 30;
+BOARD_WIDTH = 400;
+BOARD_HEIGHT = BOARD_WIDTH;
+BOARD_UNIT = BOARD_WIDTH / X_LINES;
 
 function main(){
   console.log("start");
@@ -11,6 +18,35 @@ function init(){
   cvs.width = WIDTH;
   cvs.height = HEIGHT;
   ctx = cvs.getContext('2d');
-  ctx.fillRect(40,40,500,300);
+  initBoard();
 }
 
+function initBoard() {
+  ctx.clearRect(0,0,WIDTH,HEIGHT);
+  ctx.beginPath();
+  for(var i = 0; i<= Y_LINES; i++){
+    ctx.moveTo(BOARD_OFFSET_X+i*BOARD_UNIT,BOARD_OFFSET_Y+0);
+    ctx.lineTo(BOARD_OFFSET_X+i*BOARD_UNIT,BOARD_OFFSET_Y+BOARD_HEIGHT);
+  }
+  for(var i = 0; i<= Y_LINES; i++){
+    ctx.moveTo(BOARD_OFFSET_X,BOARD_OFFSET_Y+i*BOARD_UNIT);
+    ctx.lineTo(BOARD_OFFSET_X+BOARD_WIDTH,BOARD_OFFSET_Y+i*BOARD_UNIT);
+  }
+  ctx.stroke();
+}
+
+function setStone(){
+
+}
+
+function getStone(){
+
+}
+
+function drawBoard(){
+
+}
+
+function check(){
+
+}
