b7948664151e2f7e6dcbba297d04245ebe1a560c
NNAKAYAMA <nn67101460@gmail.com>
2014-06-13 01:01:01 +0900

    6/13 changed

--- js/main.js
+++ js/main.js
@@ -1,31 +1,44 @@
 WIDTH = 640;
 HEIGHT = 480;
-X_LINES = 14;
-Y_LINES = 14;
+X_LINES = 8;
+Y_LINES = 8;
 BOARD_OFFSET_X = 30;
 BOARD_OFFSET_Y = 30;
-BOARD_WIDTH = 400;
+BOARD_WIDTH = 320;
 BOARD_HEIGHT = BOARD_WIDTH;
 BOARD_UNIT = BOARD_WIDTH / X_LINES;
+Color_Flg=true; //trueなら黒,falseなら白
+Win_Flg=true;
+Stone_Color=[];
+Stone_Num=0;  
 
 function main(){
   console.log("start");
   init();
-
-   window.onclick = function(e){
-        var cx = Math.floor(e.clientX/BOARD_UNIT) ;
-        var cy = Math.floor(e.clientY/BOARD_UNIT) ;
-        setStone(cx,cy,false);
-        console.log(cx,cy);
-    }
 }
 
 function init(){
-  cvs = document.getElementById('cvs');
-  cvs.width = WIDTH;
-  cvs.height = HEIGHT;
-  ctx = cvs.getContext('2d');
-  initBoard();
+    cvs = document.getElementById('cvs');
+    cvs.width = WIDTH;
+    cvs.height = HEIGHT;
+    ctx = cvs.getContext('2d');
+    
+    initBoard();
+    
+    window.onclick = function(e){
+      if(e.clientX%BOARD_UNIT<=BOARD_UNIT/2){
+        var cx = Math.floor(e.clientX/BOARD_UNIT);
+      }else{
+        var cx = Math.floor(e.clientX/BOARD_UNIT)+1;
+      }
+
+      if (e.clientY%BOARD_UNIT<=BOARD_UNIT/2){
+        var cy = Math.floor(e.clientY/BOARD_UNIT);
+      }else{
+        var cy = Math.floor(e.clientY/BOARD_UNIT)+1;
+      }
+      setStone(cx,cy,Color_Flg);
+    } 
 }
 
 function initBoard() {
@@ -43,33 +56,84 @@ function initBoard() {
 }
 
 function setStone(x,y,dir){
-	console.log("sS_ok");
-	if(dir){
+  if(Win_Flg){
+    if(x<=9 && y<=9 && Stone_Color[10*x+y]!="Black" && Stone_Color[10*x+y]!="White"){
+      if(dir){
+          ctx.beginPath();
+          ctx.arc(30+BOARD_UNIT*(x-1),30+BOARD_UNIT*(y-1),10,Math.PI*2,false);
+          ctx.closePath();
+          ctx.fillStyle="black";
+          ctx.fill();
+          Color_Flg=false;
+          Stone_Color[10*x+y]="Black";
+        }
+
+      else{
         ctx.beginPath();
         ctx.arc(30+BOARD_UNIT*(x-1),30+BOARD_UNIT*(y-1),10,Math.PI*2,false);
         ctx.closePath();
+        ctx.fillStyle="white";
         ctx.fill();
+        Color_Flg=true;
+        Stone_Color[10*x+y]="White";
+      }
+      Check(x,y);
+    }
+  }
+}
+    
+function Check(x,y){
+  Check_Horizontal(y);
+  Check_Vertical(x);
+  Check_Diagonal(x,y);
+}
+
+function Check_Horizontal(y){
+ for(var i=1;i<=9;i++){
+    Check_Stone_Num(i,y);
+  }
+  Stone_Num=0;
+}
+
+function Check_Vertical(x){
+  for(var i=1;i<=9;i++){
+    Check_Stone_Num(x,i);
+  }
+  Stone_Num=0;
+}
+
+function Check_Diagonal(x,y){
+  for(var i=1;i<=9;i++){
+    Check_Stone_Num(i,x+y-i);
+  }
+  Stone_Num=0;
+
+  for(var i=1;i<=9;i++){
+    Check_Stone_Num(i,y-x+i);
+  }
+  Stone_Num=0;
+}
+
+function Check_Stone_Num(x,y){
+  if(!Color_Flg){
+    if(Stone_Color[10*x+y]=="Black"){
+      Stone_Num++;
     }
     else{
-        ctx.beginPath();
-        ctx.arc(30+BOARD_UNIT*(x-1),30+BOARD_UNIT*(y-1),10,Math.PI*2,false);
-        ctx.stroke();
-
-        ctx.arc(30+BOARD_UNIT*(x-1),30+BOARD_UNIT*(y-1),7,Math.PI*2,false);
-        ctx.fillStyle = "white";
-        ctx.fill();
-        ctx.closePath();
+      Stone_Num=0;
     }
+  }
+  else{
+    if(Stone_Color[10*x+y]=="White"){
+      Stone_Num++;
+    }
+    else{
+      Stone_Num=0;
+    }
+  }
+  if(Stone_Num==5){
+    console.log(Stone_Color[10*x+y]+" WIN")
+    Win_Flg=false;
+  }
 }
 
-function getStone(){
-
-}
-
-function drawBoard(){
-
-}
-
-function check(){
-
-}
