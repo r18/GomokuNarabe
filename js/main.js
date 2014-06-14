8a5320653612d4abd2c91a2ff720934ea562e9b1
NNAKAYAMA <nn67101460@gmail.com>
2014-06-14 15:40:03 +0900

    6/14 -2

--- js/main.js
+++ js/main.js
@@ -1,42 +1,41 @@
 WIDTH = 640;
 HEIGHT = 480;
-X_LINES = 8;
-Y_LINES = 8;
+X_LINES = 14;
+Y_LINES = 14;
 BOARD_OFFSET_X = 30;
 BOARD_OFFSET_Y = 30;
-BOARD_WIDTH = 320;
+BOARD_WIDTH = 400;
 BOARD_HEIGHT = BOARD_WIDTH;
 BOARD_UNIT = BOARD_WIDTH / X_LINES;
-Color_Flg=true; //trueなら黒,falseなら白
-Win_Flg=true;
-Stone_Color=[];
+
+IS_WHITE_TURN = true;
+WIN_FLG=true;
+
+BOARD = [];
+/*
+* 1 -> WHITE
+* -1 -> BLACK
+*
+*/
 
 function main(){
-  console.log("start");
   init();
+
+    window.onclick = function(e){
+      var cx = Math.floor(e.clientX/BOARD_UNIT) ;
+      var cy = Math.floor(e.clientY/BOARD_UNIT) ;
+      if (setStone(cx,cy,IS_WHITE_TURN)) {
+        IS_WHITE_TURN = !IS_WHITE_TURN;
+      }
+    }
 }
 
 function init(){
-    cvs = document.getElementById('cvs');
-    cvs.width = WIDTH;
-    cvs.height = HEIGHT;
-    ctx = cvs.getContext('2d');
-    
-    initBoard();
-    
-    window.onclick = function(e){
-      if(e.clientX%BOARD_UNIT<=BOARD_UNIT/2){
-        var cx = Math.floor(e.clientX/BOARD_UNIT);
-      }else{
-        var cx = Math.floor(e.clientX/BOARD_UNIT)+1;
-      }
-      if (e.clientY%BOARD_UNIT<=BOARD_UNIT/2){
-        var cy = Math.floor(e.clientY/BOARD_UNIT);
-      }else{
-        var cy = Math.floor(e.clientY/BOARD_UNIT)+1;
-      }
-      setStone(cx,cy,Color_Flg);
-    } 
+  cvs = document.getElementById('cvs');
+  cvs.width = WIDTH;
+  cvs.height = HEIGHT;
+  ctx = cvs.getContext('2d');
+  initBoard();
 }
 
 function initBoard() {
@@ -46,86 +45,79 @@ function initBoard() {
     ctx.moveTo(BOARD_OFFSET_X+i*BOARD_UNIT,BOARD_OFFSET_Y+0);
     ctx.lineTo(BOARD_OFFSET_X+i*BOARD_UNIT,BOARD_OFFSET_Y+BOARD_HEIGHT);
   }
-  for(var i = 0; i<= Y_LINES; i++){
+  for(var i = 0; i<= X_LINES; i++){
     ctx.moveTo(BOARD_OFFSET_X,BOARD_OFFSET_Y+i*BOARD_UNIT);
     ctx.lineTo(BOARD_OFFSET_X+BOARD_WIDTH,BOARD_OFFSET_Y+i*BOARD_UNIT);
   }
   ctx.stroke();
+
+  for(var y = 0; y < Y_LINES; y++){
+    var r = [];
+    for(var x = 0; x < X_LINES; x++){
+      r.push(0);
+    }
+    BOARD.push(r);
+  }
 }
 
-function setStone(x,y,dir){
-  if(Win_Flg){
-    if(x<=9 && y<=9 && Stone_Color[10*x+y]!="Black" && Stone_Color[10*x+y]!="White"){
-      if(dir){
-          ctx.fillStyle="black";
-          Color_Flg=false;
-          Stone_Color[10*x+y]="Black";
-        }
-      else{
-        ctx.fillStyle="white";
-        Color_Flg=true;
-        Stone_Color[10*x+y]="White";
-      }
-       ctx.beginPath();
-      ctx.arc(30+BOARD_UNIT*(x-1),30+BOARD_UNIT*(y-1),10,Math.PI*2,false);
-      ctx.closePath();
-      ctx.fill();
+function setStone(x,y,isWhite){
+  if(WIN_FLG){
+    if(BOARD[y][x] == 0 ){
+      BOARD[y][x] = isWhite ? 1 : -1;
+      drawStone(x,y,isWhite);
       check(x,y);
+
+      return true;
+    } else {
+      console.log("there is the stone");
+      return false;
     }
   }
 }
-    
+
+function drawStone(x,y,isWhite) {
+  ctx.beginPath();
+  ctx.arc(30+BOARD_UNIT*(x-1),30+BOARD_UNIT*(y-1),10,Math.PI*2,false);
+  ctx.stroke();
+  if(isWhite){
+    ctx.fillStyle = "white";
+  } else {
+    ctx.fillStyle = "black";
+  }
+  ctx.fill();
+  ctx.closePath();
+  }
+
 function check(x,y){
-  checkHorizontal(y);
-  checkVertical(x);
-  checkDiagonal(x,y);
+  checkStone(x,y,1,0);
+  checkStone(x,y,0,1);
+  checkStone(x,y,1,1);
+  checkStone(x,y,-1,1);
 }
 
-function checkHorizontal(y){
-  //var Stone_Num=0;
-  for(var i=1;i<=9;i++){
-    checkStoneNum(i,y);
-  }
-}
+function checkStone(x,y,i,k){
+  var UP_FLG = true;
+  var DOWN_FLG=true;
+  var STONE_COUNT=1;
+  var INCRIMENT=1;
 
-function checkVertical(x){
-  //var Stone_Num=0;
-  for(var i=1;i<=9;i++){
-    checkStoneNum(x,i);
-  }
-}
-
-function checkDiagonal(x,y){
-  //var Stone_Num=0;
-  for(var i=1;i<=9;i++){
-    checkStoneNum(i,x+y-i);
-  }
- // Stone_Num=0;
-  for(var i=1;i<=9;i++){
-    checkStoneNum(i,y-x+i);
-  }
-}
-
-function checkStoneNum(x,y){
-  if(!Color_Flg){
-    if(Stone_Color[10*x+y]=="Black"){
-      Stone_Num++;
+  while(UP_FLG || DOWN_FLG){
+    if(UP_FLG && BOARD[y][x] == BOARD[y+i*INCRIMENT][x+k*INCRIMENT]){
+      STONE_COUNT++;
     }
     else{
-      Stone_Num=0;
+      UP_FLG=false;
     }
-  }
-  else{
-    if(Stone_Color[10*x+y]=="White"){
-      Stone_Num++;
+    if (DOWN_FLG && BOARD[y][x] == BOARD[y-i*INCRIMENT][x-k*INCRIMENT]) {
+      STONE_COUNT++;
     }
     else{
-      Stone_Num=0;
+      DOWN_FLG = false;
     }
+    INCRIMENT++;
   }
-  if(Stone_Num==5){
-    alert(Stone_Color[10*x+y]+" WIN")
-    Win_Flg=false;
+  if (STONE_COUNT>=5){
+    alert("win");
+    WIN_FLG = false;
   }
 }
-
